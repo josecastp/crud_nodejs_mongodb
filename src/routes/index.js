f546ff70dc3fda80bb19a4de63b703400d5b9463
josecastp <chacoper1@hotmail.com>
2021-08-20 08:43:35 +0200

    second commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const router = express.Router(); //Este método se encarga de almacenar un objeto, que luego exportaremos
 
-module.exports = router; //Exportamos router
+module.exports = router; //Exportamos router, estará disponible en cualquier parte del proyecto.
